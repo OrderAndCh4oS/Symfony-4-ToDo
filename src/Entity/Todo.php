3ac017c9440c908ad674be5d3c01e42198af84fe
Sarcoma <sarcoma@live.co.uk>
2017-12-13 23:50:40 +0000

    first commit

---
+++ src/Entity/Todo.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity
+ * @ORM\Table(name="todo")
+ */
+class Todo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $title;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $description;
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isCompleted = false;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsCompleted()
+    {
+        return $this->isCompleted;
+    }
+
+    /**
+     * @param mixed $isCompleted
+     */
+    public function setIsCompleted($isCompleted): void
+    {
+        $this->isCompleted = $isCompleted;
+    }
+}
---
+++ src/Listener/Preflight.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Listener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class Preflight implements EventSubscriberInterface
+{
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2')))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => array('onKernelRequest', 9999),
+            KernelEvents::RESPONSE => array('onKernelResponse', 9999),
+        ];
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        // Don't do anything if it's not the master request.
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+        $request = $event->getRequest();
+        $method = $request->getRealMethod();
+        if ('OPTIONS' == $method) {
+            $response = new Response();
+            $event->setResponse($response);
+        }
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        // Don't do anything if it's not the master request.
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
+    }
+}
