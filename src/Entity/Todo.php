040f146bc876f50502d65fb9082d1d38701e4fff
Sarcoma <sarcoma@live.co.uk>
2017-12-16 22:59:20 +0000

    Added Per Page attribute

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -6,7 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ApiResource()
+ * @ApiResource(attributes={"pagination_items_per_page"=5})
  * @ORM\Entity
  * @ORM\Table(name="todo")
  * @ORM\HasLifecycleCallbacks()
